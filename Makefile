607f22cc6a237530819edefb605e3412b364cc5a
David Eisinger <david.eisinger@viget.com>
2016-05-23 12:58:52 -0400

    Code organization

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 all:
-	ocamlfind ocamlc -package batteries -linkpkg -o hex_to_base64 hex_to_base64.ml
+	ocamlfind ocamlc -package batteries -linkpkg -o challenge1 challenge1.ml
 
 clean:
 	rm *.cmi *.cmo
-	rm hex_to_base64
+	rm challenge1
--- challenge1.ml
+++ challenge1.ml
@@ -9,11 +9,15 @@ let rec pairs (chars : char list) : (char * char) list =
 let int_of_hex_pair ((c1, c2) : (char * char)) : int =
   int_of_string (Printf.sprintf "0x%c%c" c1 c2)
 
-let hex_to_base64 (hex : string) : string =
+let ints_of_hex_string (hex : string) : int list =
   let hex_pairs = pairs (String.to_list hex) in
-  let ints = List.map int_of_hex_pair hex_pairs in
+  List.map int_of_hex_pair hex_pairs
+
+let hex_to_base64 (hex : string) : string =
+  let ints = ints_of_hex_string hex in
   let chars = List.map Char.chr ints in
   let str = String.of_list chars in
   Base64.str_encode str;;
 
-print_string (hex_to_base64 (Array.get Sys.argv 1))
+let () =
+  print_string (hex_to_base64 (Array.get Sys.argv 1))
